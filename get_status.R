fd17535fc7152332e44440960964bd17f9d634d8
tobiriebe <tobi-riebe@hotmail.de>
2016-08-29 18:47:17 +0200

    Create get_status.R

---
+++ get_status.R
@@ -0,0 +1,10 @@
+library(BatchExperiments)
+library(dplyr)
+
+#
+reg = loadRegistry("mytest3D-files")
+showStatus(reg)
+
+#getJobInfo(reg, findExpired(reg)) 
+#getJobInfo(reg, findErrors(reg)) 
+#getJobInfo(reg, findDone(reg)) 
